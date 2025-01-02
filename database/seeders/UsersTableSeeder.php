229a10ad875230a6f9e70f1ca53153ec2040a570
khaled-zahran <khaled.elsaeidzahran2@gmail.com>
2025-01-02 09:46:16 +0200

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,8 +16,8 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         // Define first user details
-        $fullNameAr1 = 'خالد زهران';
-        $fullNameEn1 = 'Khaled Zahran';
+        $fullNameAr1 = 'أحمد أمام';
+        $fullNameEn1 = 'Ahmed Elemam';
 
         $namePartsAr1 = explode(' ', $fullNameAr1);
         $first_name_ar1 = $namePartsAr1[0];
@@ -28,7 +28,7 @@ class UsersTableSeeder extends Seeder
         $last_name_en1 = $namePartsEn1[1] ?? '';
 
         $user1 = User::create([
-            'email' => 'khaled.elsaeidzahran@gmail.com',
+            'email' => 'ahmedelemam@gmail.com',
             'first_name_ar' => $first_name_ar1,
             'last_name_ar' => $last_name_ar1,
             'first_name_en' => $first_name_en1,
@@ -45,34 +45,6 @@ class UsersTableSeeder extends Seeder
 
         $user1->assignRole('admin');
 
-        // Define second user details
-        $fullNameAr2 = 'Mohamed Abdullah';
-        $fullNameEn2 = 'محمد عبدالله';
-
-        $namePartsAr2 = explode(' ', $fullNameAr2);
-        $first_name_ar2 = $namePartsAr2[0];
-        $last_name_ar2 = $namePartsAr2[1] ?? '';
-
-        $namePartsEn2 = explode(' ', $fullNameEn2);
-        $first_name_en2 = $namePartsEn2[0];
-        $last_name_en2 = $namePartsEn2[1] ?? '';
-
-        $user2 = User::create([
-            'email' => 'm.abdullah@nmu.edu.eg',
-            'first_name_ar' => $first_name_ar2,
-            'last_name_ar' => $last_name_ar2,
-            'first_name_en' => $first_name_en2,
-            'last_name_en' => $last_name_en2,
-            'gender' => 'male',
-            'password' => Hash::make('admin'),
-            'status' => 'active',
-            'profile_picture' => null,
-            'last_login' => now(),
-            'is_verified' => true,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-        $user2->assignRole('admin'); 
+       
     }
 }
